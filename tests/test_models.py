d37b92b0a05f6607a08feea4ad9d47738463c6d0
greatxrider <daligdigjeph09@gmail.com>
2023-09-23 00:03:42 -0400

    addressed blank lines

--- tests/test_models.py
+++ tests/test_models.py
@@ -19,6 +19,7 @@ DATABASE_URI = os.getenv(
 class TestAccount(unittest.TestCase):
     """Test Cases for Account Model"""
 
+
     @classmethod
     def setUpClass(cls):
         """This runs once before the entire test suite"""
@@ -28,19 +29,21 @@ class TestAccount(unittest.TestCase):
         app.logger.setLevel(logging.CRITICAL)
         Account.init_db(app)
 
+
     @classmethod
     def tearDownClass(cls):
         """This runs once after the entire test suite"""
+
 
     def setUp(self):
         """This runs before each test"""
         db.session.query(Account).delete()  # clean up the last tests
         db.session.commit()
 
+
     def tearDown(self):
         """This runs after each test"""
         db.session.remove()
-
     ######################################################################
     #  T E S T   C A S E S
     ######################################################################
@@ -63,6 +66,7 @@ class TestAccount(unittest.TestCase):
         self.assertEqual(account.phone_number, fake_account.phone_number)
         self.assertEqual(account.date_joined, fake_account.date_joined)
 
+
     def test_add_a_account(self):
         """It should Create an account and add it to the database"""
         accounts = Account.all()
@@ -73,6 +77,7 @@ class TestAccount(unittest.TestCase):
         self.assertIsNotNone(account.id)
         accounts = Account.all()
         self.assertEqual(len(accounts), 1)
+
 
     def test_read_account(self):
         """It should Read an account"""
@@ -86,6 +91,7 @@ class TestAccount(unittest.TestCase):
         self.assertEqual(found_account.address, account.address)
         self.assertEqual(found_account.phone_number, account.phone_number)
         self.assertEqual(found_account.date_joined, account.date_joined)
+
 
     def test_update_account(self):
         """It should Update an account"""
@@ -102,6 +108,7 @@ class TestAccount(unittest.TestCase):
         account = Account.find(account.id)
         self.assertEqual(account.email, "XYZZY@plugh.com")
 
+
     def test_delete_an_account(self):
         """It should Delete an account from the database"""
         accounts = Account.all()
@@ -117,6 +124,7 @@ class TestAccount(unittest.TestCase):
         accounts = Account.all()
         self.assertEqual(len(accounts), 0)
 
+
     def test_list_all_accounts(self):
         """It should List all Accounts in the database"""
         accounts = Account.all()
@@ -127,6 +135,7 @@ class TestAccount(unittest.TestCase):
         accounts = Account.all()
         self.assertEqual(len(accounts), 5)
 
+
     def test_find_by_name(self):
         """It should Find an Account by name"""
         account = AccountFactory()
@@ -135,6 +144,7 @@ class TestAccount(unittest.TestCase):
         same_account = Account.find_by_name(account.name)[0]
         self.assertEqual(same_account.id, account.id)
         self.assertEqual(same_account.name, account.name)
+
 
     def test_serialize_an_account(self):
         """It should Serialize an account"""
@@ -146,6 +156,7 @@ class TestAccount(unittest.TestCase):
         self.assertEqual(serial_account["address"], account.address)
         self.assertEqual(serial_account["phone_number"], account.phone_number)
         self.assertEqual(serial_account["date_joined"], str(account.date_joined))
+
 
     def test_deserialize_an_account(self):
         """It should Deserialize an account"""
@@ -160,15 +171,18 @@ class TestAccount(unittest.TestCase):
         self.assertEqual(new_account.phone_number, account.phone_number)
         self.assertEqual(new_account.date_joined, account.date_joined)
 
+
     def test_deserialize_with_key_error(self):
         """It should not Deserialize an account with a KeyError"""
         account = Account()
         self.assertRaises(DataValidationError, account.deserialize, {})
 
+
     def test_deserialize_with_type_error(self):
         """It should not Deserialize an account with a TypeError"""
         account = Account()
         self.assertRaises(DataValidationError, account.deserialize, [])
+
 
     def test_id_attribute_is_none(self):
         # Create an instance of YourClass
